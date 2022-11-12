72ec4c5d8ce8925b14b38ffde13896e821788ef2
moc <1287607885@qq.com>
2022-11-12 17:18:26 +0800

    628三个数最大乘积：线性扫描做法

--- leetcode/editor/cn/maximum-product-of-three-numbers.cpp
+++ leetcode/editor/cn/maximum-product-of-three-numbers.cpp
@@ -46,9 +46,38 @@ class Solution {
 public:
     int maximumProduct(vector<int>& nums)
     {
-        sort(nums.begin(), nums.end());
-        return max(nums[0] * nums[1] * nums[nums.size() - 1],
-                   nums[nums.size() - 1] * nums[nums.size() - 2] * nums[nums.size() - 3]);
+        int min1 = INT_MAX, min2 = INT_MAX;
+        int max1 = INT_MIN, max2 = INT_MIN, max3 = INT_MIN;
+
+        for (int i : nums)
+        {
+            if (i < min1)
+            {
+                min2 = min1;
+                min1 = i;
+            }
+            else if (i < min2)
+            {
+                min2 = i;
+            }
+
+            if (i > max1)
+            {
+                max3 = max2;
+                max2 = max1;
+                max1 = i;
+            }
+            else if (i > max2)
+            {
+                max3 = max2;
+                max2 = i;
+            }
+            else if (i > max3)
+            {
+                max3 = i;
+            }
+        }
+        return max(min1 * min2 * max1, max1 * max2 * max3);
     }
 };
 //leetcode submit region end(Prohibit modification and deletion)
