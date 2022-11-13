1815b9a1ced8c168b744bc0f9f52c685a5f2d9e2
moc <1287607885@qq.com>
2022-11-13 10:30:12 +0800

    204计数质数埃筛法未优化做法

---
+++ leetcode/editor/cn/count-primes.cpp
@@ -0,0 +1,74 @@
+/**
+给定整数 n ，返回 所有小于非负整数 n 的质数的数量 。 
+
+ 
+
+ 示例 1： 
+
+ 
+输入：n = 10
+输出：4
+解释：小于 10 的质数一共有 4 个, 它们是 2, 3, 5, 7 。
+ 
+
+ 示例 2： 
+
+ 
+输入：n = 0
+输出：0
+ 
+
+ 示例 3： 
+
+ 
+输入：n = 1
+输出：0
+ 
+
+ 
+
+ 提示： 
+
+ 
+ 0 <= n <= 5 * 10⁶ 
+ 
+
+ Related Topics 数组 数学 枚举 数论 👍 972 👎 0
+
+*/
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+public:
+    int countPrimes(int n)
+    {
+        vector<bool> isPrime(n, true);
+        int count = 0;
+        for (int i = 2; i < n; i++)
+        {
+            if (isPrime[i])
+            {
+                count++;
+                for (int j = 2 * i; j < n; j += i)
+                {
+                    isPrime[j] = false;
+                }
+            }
+        }
+        return count;
+    }
+};
+//leetcode submit region end(Prohibit modification and deletion)
+
+
+int main()
+{
+    Solution s;
+    // vector<int> data{7, 1, 5, 3, 6, 4};
+    
+    cout<<"Hello LeetCode"<<endl;
+}
