2ce7add62a76bf7a00ce9c51f349f85a81235c4a
moc <1287607885@qq.com>
2022-11-16 13:00:27 +0800

    441.排列硬币二分查找做法

--- leetcode/editor/cn/arranging-coins.cpp
+++ leetcode/editor/cn/arranging-coins.cpp
@@ -42,13 +42,25 @@ class Solution {
 public:
     int arrangeCoins(int n)
     {
-        int row = 1;
-        while (n > 0)
+        long low = 0, high = n;
+        while (low <= high)
         {
-            row++;
-            n = n - row;
+            long mid = (high - low) / 2 + low;
+            long cost = (mid * mid + mid) / 2;
+            if (cost == n)
+            {
+                return mid;
+            }
+            else if (cost > n)
+            {
+                high = mid - 1;
+            }
+            else
+            {
+                low = mid + 1;
+            }
         }
-        return row - 1;
+        return high;
     }
 };
 //leetcode submit region end(Prohibit modification and deletion)
